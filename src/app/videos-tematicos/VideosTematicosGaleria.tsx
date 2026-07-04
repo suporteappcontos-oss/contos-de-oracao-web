c09efc95bf5392c4474e3bfd6646f7c6ec9d1338
Projeto <j.p2013neto@gmail.com>
2026-07-04 14:38:19 -0300

    Ajuste proporcao 16/9 dos cards de videos tematicos

--- src/app/videos-tematicos/VideosTematicosGaleria.tsx
+++ src/app/videos-tematicos/VideosTematicosGaleria.tsx
@@ -165,7 +165,7 @@ export default function VideosTematicosGaleria({ videos }: { videos: VideoTemati
               >
                 <div
                   className="relative w-full overflow-hidden cursor-pointer"
-                  style={{ aspectRatio: '9/16' }}
+                  style={{ aspectRatio: '16/9' }}
                   onClick={() => setVideoAtualIndex(idx)}
                 >
                   <img
